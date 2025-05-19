f3d826d504ac98d56d106e1b7707f932f03ff5cb
ka3vas <tyrnis@gmail.com>
2025-05-19 13:57:49 +0200

    CH2L5:Draw Player

--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
@@ -16,6 +22,7 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
